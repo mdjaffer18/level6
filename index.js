f27f6032bd858c2472e61b3aa8cb40e78ba3c8de
jaffer <mohammadjaffer1826@gmail.com>
2023-01-05 15:30:19 +0530

    level 6 files

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const app = require("./app");
-
-app.listen(3000, () => {
-  console.log("Started express server at port 3000");
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/styles.css" rel="stylesheet">
+    <title>Todo Application</title>
+</head>
+<body>
+    <%- include('header.ejs') %> 
+    <%- include('todos.ejs') %>
+    <%-include('footer.ejs')%>
+   
+</body>
+</html>
